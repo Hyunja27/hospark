76ec2b7a9277372d649f54500d9b1fce76645d49
Hojun Park <hopark@c10r3s14.42seoul.kr>
2021-05-31 02:30:09 +0900

    0230

--- moviemon_hospark/moviemon/static/keyevent.js
+++ moviemon_hospark/moviemon/static/keyevent.js
@@ -11,11 +11,11 @@ window.addEventListener("keydown", (event) => {
   if (event.key == "ArrowRight") {
     return document.getElementById("right").click();
   }
-  if (event.key == "A") {
+  if (event.key == "A" || event.key == "a") {
     return document.getElementById("A").click();
   }
-  if (event.key == "S") {
-    return document.getElementById("S").click();
+  if (event.key == "S" || event.key == "s") {
+    return document.getElementById("B").click();
   }
   if (event.key == "Enter") {
     return document.getElementById("Select").click();
